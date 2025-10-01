b36ca03c5b10cfc42a4744e0ef9850e07044fcae
newsocialsandra <sandra.e.b.svensson@gmail.com>
2025-10-01 10:17:52 +0200

    Logged more things for second exercise

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+// P1 - testing out printing to the console
+
 console.log("Hello, World!");
 console.log("What an amazing day.");
 console.log(1+1);
 console.log(20*2);
+console.log("Can we combine strings with numbers? " + 8);
+
+// P2 - formatting doesn't matter
+
+console.log("This is line 1.");
+console.log("This is line 2.");
+console.log("This is line 3."); console.log("This is line 4.");
+console.log("This is line 5");;;
+console.log("This is line 6");
