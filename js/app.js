5ee992ac386c09830dd970d0ccd8fb3b6893e349
newsocialsandra <sandra.e.b.svensson@gmail.com>
2025-10-01 10:27:43 +0200

    Added comments for third exercise

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
+// P3 - comments in js
+
+/*
+console.log("Sandra Svensson");
+console.log("1 October 2025");
+console.log("sandra.svensson@medieinstitutet.se");
+ */
+
 // P1 - testing out printing to the console
 
 console.log("Hello, World!");
-console.log("What an amazing day.");
+console.log("What a day huh?");
 console.log(1+1);
 console.log(20*2);
 console.log("Can we combine strings with numbers? " + 8);
+console.log("Can we combine strings with strings? " + "Maybe?");
 
 // P2 - formatting doesn't matter
 
 console.log("This is line 1.");
 console.log("This is line 2.");
 console.log("This is line 3."); console.log("This is line 4.");
-console.log("This is line 5");;;
-console.log("This is line 6");
+console.log("This is line 5.");;;
+console.log("This is line 6.");
+
+
